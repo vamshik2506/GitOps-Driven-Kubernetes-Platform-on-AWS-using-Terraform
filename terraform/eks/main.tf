3a607bfa4743d7f400cf6bc1e12ca569af7ef296
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-19 09:53:48 +0530

    Update main.tf

---
+++ terraform/eks/main.tf
@@ -0,0 +1,21 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  subnets         = var.subnets
+  vpc_id          = var.vpc_id
+
+  node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = var.project
+  }
+}
