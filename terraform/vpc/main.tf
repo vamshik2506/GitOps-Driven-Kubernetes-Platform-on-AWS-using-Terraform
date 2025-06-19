460ecca8db8fe8608137b8bcbc74c5bf269c90d2
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-19 17:21:24 +0530

    Update main.tf

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -9,14 +9,16 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "public" {
-  count             = length(var.public_subnets)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.public_subnets[count.index]
-  availability_zone = var.azs[count.index]
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.azs[count.index]
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.project}-public-subnet-${count.index + 1}"
+    Name                                      = "${var.project}-public-subnet-${count.index + 1}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
   }
 }
 
@@ -26,4 +28,23 @@ resource "aws_internet_gateway" "igw" {
   tags = {
     Name = "${var.project}-igw"
   }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
