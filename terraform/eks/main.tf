c35ffe75fdb055f91d49b3d3d9b4ba75734e011a
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-19 17:19:43 +0530

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -6,14 +6,18 @@ module "eks" {
   subnet_ids      = var.subnets
   vpc_id          = var.vpc_id
 
+  iam_role_arn          = var.iam_role_arn
+  authentication_mode   = "API"
+  
+
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
+      iam_role_arn   = var.iam_role_arn
     }
   }
 
