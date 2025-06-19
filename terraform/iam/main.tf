23e80a6b23d4683975b6e87f17d91a27bd25703b
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-19 17:20:31 +0530

    Update main.tf

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -16,3 +16,28 @@ resource "aws_iam_role" "eks_cluster_role" {
     Name = "eksClusterRole"
   }
 }
+resource "aws_iam_role" "eks_node_role" {
+  name = "eksNodeRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  ])
+
+  policy_arn = each.value
+  role       = aws_iam_role.eks_node_role.name
+}
