faa67b6438803ba7f0088046c4b9ae98f44358f2
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-19 09:52:32 +0530

    Update main.tf

---
+++ terraform/vpc/main.tf
@@ -0,0 +1,28 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.project}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project}-public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project}-igw"
+  }
+}
