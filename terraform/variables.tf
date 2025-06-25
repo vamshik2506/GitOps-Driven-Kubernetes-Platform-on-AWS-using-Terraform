dfa6c2f335a2d9d72944256e664e2a29623870b0
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-25 11:03:14 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,23 @@ variable "azs" {
   type = list(string)
 }
 variable "cluster_name" {}
+
+# Optional (you can add them if you want to make things flexible)
+variable "cluster_version" {
+  default = "1.28"
+}
+
+variable "aws_auth_users" {
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+  default = [
+    {
+      userarn  = "arn:aws:iam::044854092841:user/krishna"
+      username = "krishna"
+      groups   = ["system:masters"]
+    }
+  ]
+}
