39e3a261f5a12722f363bf57be5f597ed8938805
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-25 10:43:25 +0530

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "20.8.5"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
