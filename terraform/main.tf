978221ea05872a3f50f7782189fc17f846eed14b
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-25 11:07:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,15 +12,20 @@ module "iam" {
   cluster_name = module.eks.cluster_name
 }
 module "eks" {
-  source          = "./eks"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
+
   cluster_name    = var.cluster_name
   cluster_version = "1.28"
   vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.public_subnet_ids
-  project         = var.project
-  iam_role_arn    = module.iam.eks_node_role_arn
+  subnet_ids      = module.vpc.public_subnet_ids
 
-  # ✅ Pass these to the real module
+  authentication_mode = "API"
+  iam_role_arn        = module.iam.eks_node_role_arn
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
   manage_aws_auth_configmap = true
 
   aws_auth_users = [
@@ -30,7 +35,24 @@ module "eks" {
       groups   = ["system:masters"]
     }
   ]
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+      iam_role_arn   = module.iam.eks_node_role_arn
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = var.project
+  }
 }
+
 
 module "argocd" {
   source           = "./argocd"
