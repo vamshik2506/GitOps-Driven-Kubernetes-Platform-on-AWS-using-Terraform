9abf3ecae38c850f7f1710cb0001521acce08f54
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-25 14:42:12 +0530

    Update eks-auth.tf

--- terraform/eks-auth.tf
+++ terraform/eks-auth.tf
@@ -1,14 +1,1 @@
-resource "aws_eks_access_entry" "eks_user_access" {
-  cluster_name  = module.eks.cluster_name
-  principal_arn = "arn:aws:iam::044854092841:user/krishna"
-}
 
-resource "aws_eks_access_policy_association" "admin_access" {
-  cluster_name  = module.eks.cluster_name
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = "arn:aws:iam::044854092841:user/krishna"
-
-  access_scope {
-    type = "cluster"
-  }
-}
