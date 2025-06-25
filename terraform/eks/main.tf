e429040f0b0a1ca23bd5faff908eded21e51ee9d
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-25 10:34:40 +0530

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,22 +1,29 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 20.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
   subnet_ids      = var.subnets
   vpc_id          = var.vpc_id
 
-  # ✅ Enable public and private access
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
-
-  # Optional for API access via IAM
-  iam_role_arn        = var.iam_role_arn
   authentication_mode = "API"
+  iam_role_arn        = var.iam_role_arn
 
-   eks_managed_node_groups = {
+  manage_aws_auth_configmap = true
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::044854092841:user/krishna"
+      username = "krishna"
+      groups   = ["system:masters"]
+    }
+  ]
+
+  eks_managed_node_groups = {
     default = {
       desired_size   = 2
       max_size       = 3
@@ -24,12 +31,9 @@ module "eks" {
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
       iam_role_arn   = var.iam_role_arn
-
-      # ✅ Attach custom security group
       security_groups = [aws_security_group.eks_node_sg.id]
     }
   }
-
 
   tags = {
     Environment = "dev"
