089a9e84e9a9ea4bb8775f015378280cf7706901
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-26 10:14:57 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "vamshi-terraform-backend"      # ← your bucket
+    bucket         = "vamshi-terraform-backend1"      # ← your bucket
     key            = "gitops/terraform.tfstate"       # ← a folder/key inside the bucket
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"                # ← optional, for locking
